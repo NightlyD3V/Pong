1f3bfb4d2c85709e799f56b329c214407b5d54cc
NightlyD3V <TylerSpaulding95@gmail.com>
2022-06-23 10:37:52 -0400

    Working on paddle

--- entities/headers/paddle.h
+++ entities/headers/paddle.h
@@ -6,10 +6,13 @@ class Paddle
     public: 
         int velocity_y;
         int pos_x, pos_y;
+        int height, width;
+        SDL_Rect* rect;
+        SDL_Renderer* renderer;
 
-    Paddle(int velocity_y, int pos_x, int pos_y);
+    Paddle(int velocity_y, int pos_x, int pos_y, SDL_Renderer* renderer);
 
-    void move(int x, int y);
+    void move(int pos_x, int pos_y);
 
 };
 
