d13108e1c8adeca841378828265669931836321b
NightlyD3V <TylerSpaulding95@gmail.com>
2022-06-23 11:07:13 -0400

    Working on paddle

--- entities/paddle.cpp
+++ entities/paddle.cpp
@@ -2,14 +2,25 @@
 
 int velocity _y 
 int pos_x, pos_y;
+int width, height;
 
 //-- Constructor
-Paddle::void Paddle(int velocity_y, int pos_x, int pos_y)
+Paddle::void Paddle(
+    int velocity_y, 
+    int pos_x, int pos_y, 
+    int height, int width
+    SDL_Renderer* renderer,
+    SDL_Rect* rect
+    )
 {
     printf("Paddle is being created!");
     this-> velocity_y = velocity_y;
     this-> pos_x = pos_x;
     this-> pos_y = pos_y;
+    this-> height = height;
+    this-> width = width;
+    this-> renderer = renderer;
+    this-> rect = rect;
 }
 
 //-- Methods
